3e9ac4fa7d6782f3ebcd625a8c35d974020971a2
Thorsten Fischer <thorsten@froschi.org>
2012-06-19 16:04:44 +0200

    Added depencies to 2.7 recipe.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/2.7.rb
+++ recipes/2.7.rb
@@ -1,10 +1,16 @@
+include_recipe "libbz2"
+include_recipe "libdb"
+include_recipe "libexpat"
+include_recipe "libncursesw"
+include_recipe "libreadline"
+include_recipe "libsqlite"
+include_recipe "libtinfo"
+include_recipe "mime-support"
+include_recipe "python-minimal"
+
 packages = Array.new
 
 case node[:lsb][:codename]
-when "lucid"
-  packages |= %w/
-    python2.7
-  /
 when "precise"
   packages |= %w/
     python2.7
