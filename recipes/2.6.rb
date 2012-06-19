47309c2fa497210bd440278f4912f6824fc98f57
Thorsten Fischer <thorsten@froschi.org>
2012-06-19 16:03:00 +0200

    Added dependencies to 2.6 recipe.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/2.6.rb
+++ recipes/2.6.rb
@@ -1,11 +1,15 @@
+include_recipe "libbz2"
+include_recipe "libdb"
+include_recipe "libncursesw"
+include_recipe "libreadline"
+include_recipe "libsqlite"
+include_recipe "mime-support"
+include_recipe "python-minimal"
+
 packages = Array.new
 
 case node[:lsb][:codename]
 when "lucid"
-  packages |= %w/
-    python2.6
-  /
-when "precise"
   packages |= %w/
     python2.6
   /
